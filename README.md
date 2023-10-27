e7bb81190557d15a8b6e7979de09ee697450dc5a
optmlclass <77820760+optmlclass@users.noreply.github.com>
2023-10-27 18:57:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Optimization for Machine Learning Programming Assignment 2
+
+In this assignment you will be training an image classification and a
+language modeling network using different optimization algorithms. The problem set is designed to
+completed using [google colab](colab.research.google.com/) in order to make use of the free GPU resources provided by Google.
+
+However, if you have access to a GPU in some other manner, you are certainly welcome to use that instead.
+
+To get started in colab, first download the `ProgrammingSet2.ipynb` file from this repo. Then, in colab, go to file->open notebook, choose the "upload" option and upload the downloaded file. 
+
+If you use colab, make sure you go to runtime->change runtime type, and select "GPU" so that you are using the GPU.
+
+The assignment is due on November 10th.
+
+You should not need to modify any of the python files in this repo - they just provide modeling code and a few utility functions. The entire assignment is in the .ipynb file.
---
+++ sequenceutils.py
@@ -0,0 +1,78 @@
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import math
+from torch.utils.data import Dataset
+from torch.nn import functional as F
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def top_k_logits(logits, k):
+    v, ix = torch.topk(logits, k)
+    out = logits.clone()
+    out[out < v[:, [-1]]] = -float('Inf')
+    return out
+
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+    """
+    take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
+    the sequence, feeding the predictions back into the model each time. Clearly the sampling
+    has quadratic complexity unlike an RNN that is only linear, and has a finite context window
+    of block_size, unlike an RNN that has an infinite context window.
+    """
+    block_size = model.get_block_size()
+    model.eval()
+    for k in range(steps):
+        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+        logits, _ = model(x_cond)
+        # pluck the logits at the final step and scale by temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop probabilities to only the top k options
+        if top_k is not None:
+            logits = top_k_logits(logits, top_k)
+        # apply softmax to convert to probabilities
+        probs = F.softmax(logits, dim=-1)
+        # sample from the distribution or take the most likely
+        if sample:
+            ix = torch.multinomial(probs, num_samples=1)
+        else:
+            _, ix = torch.topk(probs, k=1, dim=-1)
+        # append to the sequence and continue
+        x = torch.cat((x, ix), dim=1)
+
+    return x
+
+
+# define a dataset class to process the textfile in pytorch.
+
+class CharDataset(Dataset):
+
+    def __init__(self, data, block_size):
+        chars = list(set(data))
+        data_size, vocab_size = len(data), len(chars)
+        print('data has %d characters, %d unique.' % (data_size, vocab_size))
+
+        self.stoi = { ch:i for i,ch in enumerate(chars) }
+        self.itos = { i:ch for i,ch in enumerate(chars) }
+        self.block_size = block_size
+        self.vocab_size = vocab_size
+        self.data = data
+
+    def __len__(self):
+        return math.ceil(len(self.data) / (self.block_size + 1))
+
+    def __getitem__(self, idx):
+        # we're actually going to "cheat" and pick a spot in the dataset at random
+        i = np.random.randint(0, len(self.data) - (self.block_size + 1))
+        chunk = self.data[i:i+self.block_size+1]
+        dix = [self.stoi[s] for s in chunk]
+        x = torch.tensor(dix[:-1], dtype=torch.long)
+        y = torch.tensor(dix[1:], dtype=torch.long)
+        return x, y
+
